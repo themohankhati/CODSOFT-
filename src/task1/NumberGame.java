8bd95683587034bdef6ca4025d9a0cc1233fe6f9
themohankhati <mohankc596@gmail.com>
2024-09-03 21:28:57 +0545

    task1

--- src/task1/NumberGame.java
+++ src/task1/NumberGame.java
@@ -4,24 +4,53 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGame {
-    public static void main(String[] args)
-    {
-        //         generating random number
-        Random randNum=new Random();
-
-        int randomNumber=randNum.nextInt(100)+1;
-
-//            print the generated random number
-        System.out.print("Random number: " +randomNumber+ '\n');
-
-
-//      scanner object
+    public static void main(String[] args) {
+        //scanner object
         Scanner sc = new Scanner(System.in);
-        System.out.println("Guess the number between 1 and 100!");
 
-        String guessNumber= sc.nextLine();
-        System.out.println("User guess:"+ guessNumber);
+        //generating random number
+        Random randNum = new Random();
+        int rounds = 0;
+        int totalScore = 0;
+        String playAgain;
 
+        do {
+            rounds++;
+            int randomNumber = randNum.nextInt(100) + 1;
+            int numOfAttempts = 0;
+            int maxAttempts = 5;
+            boolean hasWon = false;
 
-    };
-    }
+            System.out.println("Round" + rounds + " :Guess the number between 1 and 100!");
+        while (numOfAttempts < maxAttempts) {
+                System.out.println("Guess the number between 1 and 100!");
+                int userGuess = sc.nextInt();
+                numOfAttempts++;
+
+                if (userGuess == randomNumber) {
+                    hasWon = true;
+                } else if (userGuess > randomNumber) {
+                    System.out.println("Too high!");
+                } else if (userGuess < randomNumber) {
+                    System.out.println("Too low!");
+
+                }
+
+            }
+
+        if(hasWon){
+            System.out.println("Congratulations! you've guessed the number in " +numOfAttempts+ "attempts.");
+            totalScore+= (maxAttempts-numOfAttempts+1);
+        }
+        else {
+            System.out.println("You've used all your attempts. The correct number was " + randomNumber + ".");
+        }
+
+        System.out.println("Do you want to play again? (yes or no)");
+        playAgain=sc.next();
+
+        } while (playAgain.equalsIgnoreCase("yes"));
+        System.out.println("So Game is over! you've played"+ rounds
+        + "rounds with a total score of"+ totalScore + ".");
+
+    }}
